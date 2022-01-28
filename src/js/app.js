ae5ddeaf01a61ebb0dc3e2a2841a4927a27deb3f
Andrii <Luckyhidalgo@gmail.com>
2022-01-28 13:17:33 +0200

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+import Common from "./common/common";
+
+
+// EVENT LISTENER - LOAD
+// ========================================
+window.addEventListener('load', (ev) => {
+  
+  // COMMON
+  Common.initLoad();
+  
+  // MACROS
+  
+}, false);
+
+
+// EVENT LISTENER - SCROLL
+// ========================================
+window.addEventListener('scroll', (ev) => {}, false);
